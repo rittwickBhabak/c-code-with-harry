bd0f149bfdc03cf4f1758da650fb811506e5ec89
Rittwick Bhabak <rittwick2019@gmail.com>
2022-02-06 00:59:46 +0530

    final-commit

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+    path('lesson/<int:pk>', views.detail_update_lesson, name='lesson'),
+    path('save-notes/<int:pk>', views.save_notes, name='save-notes'),
+]
